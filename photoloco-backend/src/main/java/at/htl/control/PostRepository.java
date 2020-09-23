b1628eec3eee3c45836a6d2579d7361bcffda318
tealeon <leon.tea@hotmail.com>
2020-09-23 19:09:34 +0200

    create repository

---
+++ photoloco-backend/src/main/java/at/htl/control/PostRepository.java
@@ -0,0 +1,10 @@
+package at.htl.control;
+
+import at.htl.entity.Post;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PostRepository implements PanacheRepository<Post> {
+}
