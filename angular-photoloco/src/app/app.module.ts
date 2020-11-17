a7d438bef42e93016be24777f338c102e58b8f73
tealeon <leon.tea@hotmail.com>
2020-11-17 18:11:15 +0100

    add navbar

--- angular-photoloco/src/app/app.module.ts
+++ angular-photoloco/src/app/app.module.ts
@@ -8,15 +8,27 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomeComponent } from './modules/home/components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './core/header/header.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
