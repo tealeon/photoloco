c1688bff1e5cf8da33fd7c7a44cf57b7928015ac
franziskusehmeir <f.ehmeir@gmail.com>
2020-12-07 17:35:32 +0100

    GET user by id

--- photoloco-backend/src/main/java/at/htl/photoloco/boundary/UserResource.java
+++ photoloco-backend/src/main/java/at/htl/photoloco/boundary/UserResource.java
@@ -3,12 +3,10 @@ package at.htl.photoloco.boundary;
 import at.htl.photoloco.dto.UserDto;
 import at.htl.photoloco.entity.User;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,5 +21,16 @@ public class UserResource {
         List<UserDto> users = User.streamAll().map(user -> (User) user).map(UserDto::new).collect(Collectors.toList());
 
         return Response.ok(users).build();
+    }
+
+    @GET
+    @Path("/{user-id}")
+    public Response getUserById(@PathParam("user-id") Long userId) {
+        User user = User.findById(userId);
+        if (user == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(new UserDto(user)).build();
     }
 }
