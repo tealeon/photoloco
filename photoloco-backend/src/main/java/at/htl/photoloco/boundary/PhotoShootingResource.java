b0f307b25cfe39ff157e4f02e16bde128fb4f117
franziskusehmeir <f.ehmeir@gmail.com>
2020-12-07 20:36:48 +0100

    Update PhotoShootingResource

---
+++ photoloco-backend/src/main/java/at/htl/photoloco/boundary/PhotoShootingResource.java
@@ -0,0 +1,11 @@
+package at.htl.photoloco.boundary;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//@Path("photoshoot")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PhotoShootingResource {
+}
