de2bbd7a067997876285251abe1d5ef8a767c4c2
franziskusehmeir <f.ehmeir@gmail.com>
2020-12-10 00:11:10 +0100

    add basic logic for times component

--- angular-photoloco/src/app/modules/times/times/times.component.ts
+++ angular-photoloco/src/app/modules/times/times/times.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../../../core/services/http.service';
 
 @Component({
   selector: 'app-times',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimesComponent implements OnInit {
 
-  constructor() { }
+  times: { [key: string]: any };
+  private lat: number;
+  private lng: number;
 
-  ngOnInit(): void {
+  constructor(private _http: HttpService) {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  getTimes() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this._http.getTimes(this.lat, this.lng).subscribe(data => {
+          this.times = data;
+          console.log(this.times?.results.sunrise);
+        });
+      });
+    } else {
+      console.log('User not allow');
+    }
+  }
 }
