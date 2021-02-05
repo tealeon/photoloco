6c98281d867f707ae8f8a512ef3df4b7f8eda96e
tealeon <leon.tea@hotmail.com>
2021-02-05 12:08:17 +0100

    create location-list-component

---
+++ angular-photoloco/src/app/modules/user/components/location-list/location-list.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {LocationModel} from '../../../../shared/models/location.model';
+import {LocationService} from '../../../../core/services/location.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-location-list',
+  templateUrl: './location-list.component.html',
+  styleUrls: ['./location-list.component.css']
+})
+export class LocationListComponent implements OnInit {
+
+  locations: Array<LocationModel>;
+
+  constructor(
+    private router: Router,
+    private locationService: LocationService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.locationService.getLocations().subscribe(
+      data => {
+        this.locations = data;
+      }
+    );
+  }
+
+  getAllLocations(): Array<LocationModel> {
+    return this.locations;
+  }
+
+}
