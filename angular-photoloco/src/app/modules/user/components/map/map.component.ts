476101c8c8d4714fa91e3e4e03a53edd077c8dfe
tealeon <leon.tea@hotmail.com>
2021-02-05 12:58:09 +0100

    create map-component

---
+++ angular-photoloco/src/app/modules/user/components/map/map.component.ts
@@ -0,0 +1,34 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+
+  private map;
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    this.initMap();
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+  }
+
+}
