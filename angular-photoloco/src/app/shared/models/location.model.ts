3bb4ff2cd5a00ddd2285512d3de9d7468552a5ea
tealeon <leon.tea@hotmail.com>
2021-02-10 12:06:17 +0100

    update model

--- angular-photoloco/src/app/shared/models/location.model.ts
+++ angular-photoloco/src/app/shared/models/location.model.ts
@@ -4,13 +4,11 @@ export class LocationModel {
   name: string;
   longitude: number;
   latitude: number;
-  locations: object;
 
-  constructor(id: number, name: string, longitude: number, latitude: number, locations: object) {
+  constructor(id: number, name: string, longitude: number, latitude: number) {
     this.id = id;
     this.name = name;
     this.longitude = longitude;
     this.latitude = latitude;
-    this.locations = locations;
   }
 }
