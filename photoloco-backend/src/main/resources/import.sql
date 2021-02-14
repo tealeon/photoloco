fdafdf2ec990a80735830ff8d101d2228ef00910
franziskusehmeir <f.ehmeir@gmail.com>
2021-02-14 02:41:45 +0100

    add users to import.sql

--- photoloco-backend/src/main/resources/import.sql
+++ photoloco-backend/src/main/resources/import.sql
@@ -15,3 +15,19 @@ insert into location(id, longitude, latitude, name)
 values (7, 47.26266, 11.39454, 'Innsbruck');
 insert into location(id, longitude, latitude, name)
 values (8, 48.25628, 13.04343, 'Braunau am Inn');
+
+/*users*/
+insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
+values (1, 'photographer without regrets', 'Linz', 'Austria', 'ltea@mail.com', 'Leon', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/124188864_879588679243345_2016517700226628149_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=jxwmT_BID8QAX-SrS_Z&tp=1&oh=62d724ca21bd4192f7175d5bd0e5783a&oe=6051B5A7', 'leon.tea', false, true, 'Tea', 'leonn', 'www.google.com');
+
+insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
+values (2, '📷 | 🌍 | 🇦🇹 | 📍 Linz ✉️ DM for photoshooting', 'Linz', 'Austria', 'fran@mail.com', 'Francis', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/121616104_1772376002918196_6595898946490198361_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=cmmWRENo3fkAX-dQlD3&tp=1&oh=023a208beafc6e792f4c99811b087d8d&oe=60511EBD', 'francis_liger', false, true, 'Ehmeir', 'franciss', 'www.google.com');
+
+insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
+values (3, '🦄', 'Linz', 'Austria', 'loo@mail.com', 'Lorenz', 'https://www.borgleon.at/wp-content/uploads/2015/10/default-user-image.png', 'l.0o0o0', false, true, 'Gruber', 'Loo', 'www.google.com');
+
+insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
+values (4, '🕊 𝘑𝘰𝘩𝘯 𝟣𝟥:𝟩', 'Linz', 'Austria', 'marxa@mail.com', 'Maria', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/146946455_482994266021683_5282674739716486629_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=JUYYySgg3AAAX_D1Q5L&tp=1&oh=d2a2c09a7aed0f044f93bc2882316f56&oe=604E9F56', 'marxamlc', true, true, 'Milic', 'MΛЯIΛ', 'www.google.com');
+
+insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
+values (5, '', 'Linz', 'Austria', 'hope@mail.com', 'Hope', 'https://www.borgleon.at/wp-content/uploads/2015/10/default-user-image.png', 'hope_snr', true, false, 'Senegal', 'Hopee', 'www.google.com');
