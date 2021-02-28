e5e5012957a916a449453957b053a7adf77e0925
tealeon <leon.tea@hotmail.com>
2021-02-28 02:35:23 +0100

    change id of locations

--- photoloco-backend/src/main/resources/import.sql
+++ photoloco-backend/src/main/resources/import.sql
@@ -1,20 +1,20 @@
 /*locations*/
 insert into location(id, longitude, latitude, name)
-values (1, 48.30639, 14.28611, 'Linz');
+values (1001, 48.30639, 14.28611, 'Linz');
 insert into location(id, longitude, latitude, name)
-values (2, 48.210033, 16.363449, 'Wien');
+values (1002, 48.210033, 16.363449, 'Wien');
 insert into location(id, longitude, latitude, name)
-values (3, 47.076668, 15.421371, 'Graz');
+values (1003, 47.076668, 15.421371, 'Graz');
 insert into location(id, longitude, latitude, name)
-values (4, 48.27965, 14.2533, 'Leonding');
+values (1004, 48.27965, 14.2533, 'Leonding');
 insert into location(id, longitude, latitude, name)
-values (5, 48.20969, 14.29004, 'Ansfelden');
+values (1005, 48.20969, 14.29004, 'Ansfelden');
 insert into location(id, longitude, latitude, name)
-values (6, 47.79941, 13.04399, 'Salzburg');
+values (1006, 47.79941, 13.04399, 'Salzburg');
 insert into location(id, longitude, latitude, name)
-values (7, 47.26266, 11.39454, 'Innsbruck');
+values (1007, 47.26266, 11.39454, 'Innsbruck');
 insert into location(id, longitude, latitude, name)
-values (8, 48.25628, 13.04343, 'Braunau am Inn');
+values (1008, 48.25628, 13.04343, 'Braunau am Inn');
 
 /*users*/
 insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
