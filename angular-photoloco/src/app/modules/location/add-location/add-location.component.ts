55d7ee61c29a16e89ec8840e52991bde6f28ae3e
tealeon <leon.tea@hotmail.com>
2021-02-28 02:36:01 +0100

    add post request

--- angular-photoloco/src/app/modules/location/add-location/add-location.component.ts
+++ angular-photoloco/src/app/modules/location/add-location/add-location.component.ts
@@ -2,6 +2,8 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {LocationService} from '../../../core/services/location.service';
 import * as L from 'leaflet';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-add-location',
@@ -16,15 +18,18 @@ export class AddLocationComponent implements OnInit, AfterViewInit {
     description: [null, Validators.required]
   });
 
-  private map;
+  private map1;
+  private url = 'http://localhost:8080/location';
 
   constructor(
     private fb: FormBuilder,
-    private locationService: LocationService
+    private locationService: LocationService,
+    private http: HttpClient
   ) {
   }
 
-  onSubmit() {
+  onSubmit(data) {
+    this.createNewLocation(data);
     alert('Thanks!');
   }
 
@@ -39,22 +44,30 @@ export class AddLocationComponent implements OnInit, AfterViewInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
-    tiles.addTo(this.map);
+    tiles.addTo(this.map1);
 
-    this.map.addEventListener('click', this.getCoords.bind(this));
+    this.map1.addEventListener('click', this.getCoords.bind(this));
   }
 
   private getCoords(ev) {
-    const latlng = this.map.mouseEventToLatLng(ev.originalEvent);
+    const latlng = this.map1.mouseEventToLatLng(ev.originalEvent);
     this.addressForm.controls.latitude.setValue(latlng.lat);
     this.addressForm.controls.longitude.setValue(latlng.lng);
     console.log(latlng.lat + ', ' + latlng.lng);
   }
 
   private initMap(): void {
-    this.map = L.map('map', {
+    this.map1 = L.map('map1', {
       center: [47.6965, 13.3457],
       zoom: 7
     });
+  }
+
+  createNewLocation(data) {
+    return this.http.post(this.url, data).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
   }
 }
