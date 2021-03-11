1b0a501292a420cb7be2b497a99b79600093f975
franziskusehmeir <f.ehmeir@gmail.com>
2021-03-11 22:58:09 +0100

    refactor photoshooting component

--- angular-photoloco/src/app/modules/photoshooting/add-photoshooting/add-photoshooting.component.ts
+++ angular-photoloco/src/app/modules/photoshooting/add-photoshooting/add-photoshooting.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-photoshooting',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPhotoshootingComponent implements OnInit {
 
+  options: string[] = ['Angular', 'React', 'Vue'];
+
+  objectOptions = [
+    { name: 'Leonn'},
+    { name: 'Franciss'},
+    { name: 'Franciss'},
+    { name: 'Loo'},
+    { name: 'MΛЯIΛ'},
+    { name: 'Hopee'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayFn(subject) {
+    return subject ? subject.name : undefined;
   }
 
 }
