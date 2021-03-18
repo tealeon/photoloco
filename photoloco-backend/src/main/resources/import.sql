d286775610d8512a7f1bdfa25e6f281e90351dfa
franziskusehmeir <f.ehmeir@gmail.com>
2021-03-18 04:10:53 +0100

    change images in import.sql

--- photoloco-backend/src/main/resources/import.sql
+++ photoloco-backend/src/main/resources/import.sql
@@ -18,19 +18,19 @@ values (1008, 48.25628, 13.04343, 'Braunau am Inn', 'city in Austria','nature �
 
 /*users*/
 insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
-values (1, 'photographer without regrets', 'Linz', 'Austria', 'ltea@mail.com', 'Leon', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/124188864_879588679243345_2016517700226628149_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=jxwmT_BID8QAX-SrS_Z&tp=1&oh=62d724ca21bd4192f7175d5bd0e5783a&oe=6051B5A7', 'leon.tea', false, true, 'Tea', 'leonn', 'www.google.com');
+values (1, 'photographer without regrets', 'Linz', 'Austria', 'ltea@mail.com', 'Leon', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/124188864_879588679243345_2016517700226628149_n.jpg?tp=1&_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=hsY39r4TEOMAX9acdj8&ccb=7-4&oh=b2d269088fea6b9688d1f60fac850f8e&oe=607D3727', 'leon.tea', false, true, 'Tea', 'leonn', 'www.google.com');
 
 insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
-values (2, '📷 | 🌍 | 🇦🇹 | 📍 Linz ✉️ DM for photoshooting', 'Linz', 'Austria', 'fran@mail.com', 'Francis', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/121616104_1772376002918196_6595898946490198361_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=cmmWRENo3fkAX-dQlD3&tp=1&oh=023a208beafc6e792f4c99811b087d8d&oe=60511EBD', 'francis_liger', false, true, 'Ehmeir', 'franciss', 'www.google.com');
+values (2, '📷 | 🌍 | 🇦🇹 | 📍 Linz ✉️ DM for photoshooting', 'Linz', 'Austria', 'fran@mail.com', 'Francis', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/121616104_1772376002918196_6595898946490198361_n.jpg?tp=1&_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=zTm9_ObQVp0AX9PYwyK&ccb=7-4&oh=3ddde37c38657078bc2927a83c2ca720&oe=607CA03D', 'francis_liger', false, true, 'Ehmeir', 'franciss', 'www.google.com');
 
 insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
-values (3, '🦄', 'Linz', 'Austria', 'loo@mail.com', 'Lorenz', 'https://www.borgleon.at/wp-content/uploads/2015/10/default-user-image.png', 'l.0o0o0', false, true, 'Gruber', 'Loo', 'www.google.com');
+values (3, '🦄', 'Linz', 'Austria', 'loo@mail.com', 'Lorenz', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', 'l.0o0o0', false, true, 'Gruber', 'Loo', 'www.google.com');
 
 insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
-values (4, '🕊 𝘑𝘰𝘩𝘯 𝟣𝟥:𝟩', 'Linz', 'Austria', 'marxa@mail.com', 'Maria', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/146946455_482994266021683_5282674739716486629_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=JUYYySgg3AAAX_D1Q5L&tp=1&oh=d2a2c09a7aed0f044f93bc2882316f56&oe=604E9F56', 'marxamlc', true, true, 'Milic', 'MΛЯIΛ', 'www.google.com');
+values (4, '🕊 𝘑𝘰𝘩𝘯 𝟣𝟥:𝟩', 'Linz', 'Austria', 'marxa@mail.com', 'Maria', 'https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/160993473_737637333606873_7043829562003434334_n.jpg?tp=1&_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=_kVjC8oyuJIAX8bzW3B&ccb=7-4&oh=90d358c7043893182e9a930a9ed8f2ca&oe=607BE578', 'marxamlc', true, true, 'Milic', 'MΛЯIΛ', 'www.google.com');
 
 insert into _user (id, biography, city, country, email, firstname, imageurl, instagramname, ismodel, isphotographer, lastname, username, websiteurl)
-values (5, '', 'Linz', 'Austria', 'hope@mail.com', 'Hope', 'https://www.borgleon.at/wp-content/uploads/2015/10/default-user-image.png', 'hope_snr', true, false, 'Senegal', 'Hopee', 'www.google.com');
+values (5, '', 'Linz', 'Austria', 'hope@mail.com', 'Hope', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', 'hope_snr', true, false, 'Senegal', 'Hopee', 'www.google.com');
 
 /*posts*/
 insert into post (id, dateofcreation, markdowncontent, title, author_id)
