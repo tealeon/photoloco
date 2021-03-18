6da0831d53097374d6a9309591a76bb924f17b12
franziskusehmeir <f.ehmeir@gmail.com>
2021-03-18 03:37:04 +0100

    add ranking style to users in explore tab

--- angular-photoloco/src/app/modules/explore/explore/explore.component.ts
+++ angular-photoloco/src/app/modules/explore/explore/explore.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../../core/services/user.service";
 import {UserModel} from "../../../shared/models/user.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-explore',
@@ -13,7 +14,8 @@ export class ExploreComponent implements OnInit {
   models: UserModel[] = [];
 
   constructor(
-    private userSerive: UserService
+    private userSerive: UserService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -33,5 +35,9 @@ export class ExploreComponent implements OnInit {
         .slice(0, 3);
       console.log(this.photographers, this.models);
     });
+  }
+
+  navigateTouserDetail(instagramName: string): void {
+    this.router.navigate(['user-detail', instagramName]);
   }
 }
