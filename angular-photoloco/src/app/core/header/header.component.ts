a08d99036058547b89ae02e466d318c8e7711ec1
franziskusehmeir <f.ehmeir@gmail.com>
2021-04-13 21:23:45 +0200

    update header

--- angular-photoloco/src/app/core/header/header.component.ts
+++ angular-photoloco/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: boolean;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+     this.userService.isLoggedIn().subscribe(isLoggedIn => {
+       this.loggedIn = isLoggedIn;
+     });
   }
 
 }
