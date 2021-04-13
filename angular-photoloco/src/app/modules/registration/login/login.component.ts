c8d87216291bfc593d89d0ced23271e2d85e1079
franziskusehmeir <f.ehmeir@gmail.com>
2021-04-13 21:05:51 +0200

    implement login component

--- angular-photoloco/src/app/modules/registration/login/login.component.ts
+++ angular-photoloco/src/app/modules/registration/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../../core/services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.fb.group({
+    instagramName: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required)
+  });
+
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+       this.userService.login(this.loginForm.value);
+    }
+  }
 }
