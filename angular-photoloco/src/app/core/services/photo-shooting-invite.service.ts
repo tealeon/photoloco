abf814e543cb0fc089afa770a11ba3c7ffcfec1d
franziskusehmeir <f.ehmeir@gmail.com>
2021-04-16 11:21:36 +0200

    add service for invite

---
+++ angular-photoloco/src/app/core/services/photo-shooting-invite.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PhotoShootingInviteModel} from '../../shared/models/photoShootingInvite.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoShootingInviteService {
+
+  url = 'http://localhost:8080/invites';
+
+  constructor(private http: HttpClient
+  ) { }
+
+  getInvitesOfUser(instagramName: string): Observable<PhotoShootingInviteModel[]> {
+    return this.http.get<PhotoShootingInviteModel[]>(this.url + '/' + instagramName);
+  }
+
+  respondToInvite(id: number, response: boolean): void {
+    this.http.get(this.url + '/' + id + '/' + response);
+  }
+
+  sendInvite(invite: PhotoShootingInviteModel): Observable<PhotoShootingInviteModel> {
+    return this.http.post<PhotoShootingInviteModel>(this.url, invite);
+  }
+
+}
